ad937ca544b194c81e557b36fc0d760ec4c68212
Richard Liu <richy.liu.2002@gmail.com>
2020-09-23 13:47:35 -0700

    update profiles, add profile pictures

--- web/src/components/about/AboutPage.js
+++ web/src/components/about/AboutPage.js
@@ -16,6 +16,56 @@ const LoadProfileData = ({ withProfileData }) => (
             }
           }
         }
+
+        nathan: file(
+          relativePath: { eq: "images/nathan.jpg" }
+        ) {
+          childImageSharp {
+            fixed(width: 224) {
+              ...GatsbyImageSharpFixed
+            }
+          }
+        }
+
+        abhinav: file(
+          relativePath: { eq: "images/abhinav.jpg" }
+        ) {
+          childImageSharp {
+            fixed(width: 224) {
+              ...GatsbyImageSharpFixed
+            }
+          }
+        }
+
+        anika: file(
+          relativePath: { eq: "images/anika.jpg" }
+        ) {
+          childImageSharp {
+            fixed(width: 224) {
+              ...GatsbyImageSharpFixed
+            }
+          }
+        }
+
+        jay: file(
+          relativePath: { eq: "images/jay.jpg" }
+        ) {
+          childImageSharp {
+            fixed(width: 224) {
+              ...GatsbyImageSharpFixed
+            }
+          }
+        }
+
+        richard: file(
+          relativePath: { eq: "images/richard.jpg" }
+        ) {
+          childImageSharp {
+            fixed(width: 224) {
+              ...GatsbyImageSharpFixed
+            }
+          }
+        }
       }
     `}
     render={data =>
@@ -24,7 +74,7 @@ const LoadProfileData = ({ withProfileData }) => (
           name: 'Eric Zhang',
           job: 'President',
           description:
-            "Eric is a senior at Monta Vista High School. He is a 2018 and 2020 USACO Finalist, a member of the winning team of Stanford ProCo's 2019 Speed Round, and a three-time AIME qualifier. In his free time, he enjoys learning about ML as well as teaching at AlphaStar Academy.",
+            "Eric is a senior at Monta Vista High School. He is a 2018 and 2020 USACO Finalist, a member of the winning team of Stanford ProCo’s 2019 Speed Round, and a three-time AIME qualifier. In his free time, he enjoys learning about ML as well as teaching at AlphaStar Academy.",
           image: <Img fixed={data.default_profile.childImageSharp.fixed} />,
         },
         {
@@ -32,35 +82,35 @@ const LoadProfileData = ({ withProfileData }) => (
           job: 'President',
           description:
             'Nathan is currently a sophomore at Monta Vista High School. He is a 2 time AIME qualifier and a USACO Platinum Contestant since his 7th grade. Nathan has qualified for USACO camp, and is additionally an active member of School Leadership.',
-          image: <Img fixed={data.default_profile.childImageSharp.fixed} />,
+          image: <Img fixed={data.nathan.childImageSharp.fixed} />,
         },
         {
           name: 'Abhinav Kommula',
           job: '',
           description:
             'Abhinav is a current senior at Monta Vista High School. In addition to being a three-time AIME qualifier, Abhinav is a USACO Platinum Contestant and a Division 1 contestant on codeforces. Apart from competitions, he’s also interested in conducting CS research within Machine Learning and Cybersecurity. In his free time, Abhinav enjoys cooking, playing pop music on the piano, and contemplating life.',
-          image: <Img fixed={data.default_profile.childImageSharp.fixed} />,
+          image: <Img fixed={data.abhinav.childImageSharp.fixed} />,
         },
         {
           name: 'Anika Jain',
           job: '',
           description:
             'Anika is a current senior at Monta Vista High School. She is a USACO Platinum Contestant and a Technovation international semifinalist. In her free time, she enjoys reading about the recent breakthroughs in astronomy, developing apps, as well as teaching coding to elementary school kids.',
-          image: <Img fixed={data.default_profile.childImageSharp.fixed} />,
+          image: <Img fixed={data.anika.childImageSharp.fixed} />,
         },
         {
           name: 'Jay Yeung',
           job: '',
           description:
-            'Jay is a sophomore at MVHS. He participated and organized Hack Cupertino and is in USACO Silver. In his free time, he enjoys playing minecraft, scouting with Troop 452, and playing piano.',
-          image: <Img fixed={data.default_profile.childImageSharp.fixed} />,
+            'Jay is a sophomore at MVHS. He participated and organized <Hack> Cupertino and is in USACO Silver. In his free time, he enjoys playing minecraft, scouting with Troop 452, and playing piano.',
+          image: <Img fixed={data.jay.childImageSharp.fixed} />,
         },
         {
           name: 'Richard Liu',
           job: 'Webmaster',
           description:
             'Richard is a senior at Monta Vista High School. He participated in FLL and FRC and is in USACO Gold. He is part of BSA Troop 485 and programs in his free time.',
-          image: <Img fixed={data.default_profile.childImageSharp.fixed} />,
+          image: <Img fixed={data.richard.childImageSharp.fixed} />,
         },
       ])
     }
